22fa2410dc7997e8e19a1e3fce0eb62632125486
friedenberg <1baggzpr@starbrandshoes.com>
2026-05-29 08:27:07 -0400

    refresh flake.lock + cut nixpkgs input → igloo

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Package framework for bundling CLIs, MCP servers, and skills";
 
   inputs = {
-    nixpkgs.url = "github:amarbel-llc/nixpkgs";
+    nixpkgs.url = "github:amarbel-llc/igloo";
     nixpkgs-master.url = "github:NixOS/nixpkgs/d233902339c02a9c334e7e593de68855ad26c4cb";
     utils.url = "https://flakehub.com/f/numtide/flake-utils/0.1.102";
 
